2c7043db2ab1a941e6795fad28c2598fc12fe624
chenle <chenle@forchange.tech>
2020-08-10 18:43:08 +0800

    feat: add 322. 零钱兑换

---
+++ 动态规划/322. 零钱兑换.js
@@ -0,0 +1,15 @@
+var coinChange = function(coins, amount) {
+  if (coins.length === 0) return -1;
+  let dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i < dp.length; i++) {
+    for (let coin of coins) {
+      if (i >= coin) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 6))
