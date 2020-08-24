96d56b3c9574bc6c3b05a0f9c69851fe4a948bfa
chenle <chenle@forchange.tech>
2020-08-24 15:43:59 +0800

    feat: 376. 摆动序列

--- 贪心算法/376. 摆动序列.js
+++ 贪心算法/376. 摆动序列.js
@@ -21,4 +21,25 @@ var wiggleMaxLength = function(nums) {
   return res + 1
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 另一种写法
+var wiggleMaxLength = function(nums) {
+  if (nums.length < 2) return nums.length
+  let up = 1,
+    down = 1
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1
+    }
+    if (nums[i] < nums[i - 1]) {
+      down = up + 1
+    }
+  }
+  return Math.max(up, down)
+};
+
 console.log(wiggleMaxLength([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]))
