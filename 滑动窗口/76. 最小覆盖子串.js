69859300ebbf3f34ab0ab33e18521201e9451219
chenle <chenle@forchange.tech>
2020-08-24 23:27:59 +0800

    feat: 76. 最小覆盖子串

--- 滑动窗口/76. 最小覆盖子串.js
+++ 滑动窗口/76. 最小覆盖子串.js
@@ -12,37 +12,44 @@ var minWindow = function(s, t) {
     }
     need[item]++;
   }
-  let left = 0,
-    right = 0,
-    valid = 0,
-    len = Infinity,
-    start;
+
+  let valid = 0;
+  let left = 0;
+  let right = 0;
+  let len = Infinity;
+  let start;
   while (right < s.length) {
+    // c 是将移入窗口的字符
     let c = s[right];
+
+    // 右移窗口
     right++;
+    // 进行窗口内数据的一系列更新
     if (need[c]) {
-      if (windows[c] === null) {
+      if (windows[c] == null) {
         windows[c] = 0;
       }
       windows[c]++;
-      if (windows[c] === need[c]) {
-        valid++;
-      }
+      if (windows[c] === need[c]) valid++;
     }
-    while (valid === Object.keys(need).length) {
+    // 判断左侧窗口是否要收缩
+    while (valid == Object.keys(need).length) {
+      // 在这里更新最小覆盖子串
       if (right - left < len) {
         start = left;
         len = right - left;
       }
+      // d 是将移出窗口的字符
       let d = s[left];
       left++;
       if (need[d]) {
-        if (windows[d] === need[d]) {
+        if (windows[d] == need[d]) {
           valid--;
         }
         windows[d]--;
       }
     }
   }
-  return len === Infinity ? "" : s.slice(start, start + len);
+
+  return len == Infinity ? "" : s.slice(start, start + len);
 };
