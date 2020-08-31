1b68f96d604cbdbedf7a15c60095f3e6334ba6de
chenle <chenle@forchange.tech>
2020-08-31 10:28:21 +0800

    feat: 841. 钥匙和房间

--- dfs/841. 钥匙和房间.js
+++ dfs/841. 钥匙和房间.js
@@ -12,13 +12,34 @@ var canVisitAllRooms = function(rooms) {
   function dfs(keys, rooms) {
     for (let key of keys) {
       if (!dp[key]) {
-        console.log(key, dp, rooms[key])
         dp[key] = true
         dfs(rooms[key], rooms)
       }
     }
   }
   return !dp.some(item => !item)
+};
+
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+  let length = rooms.length
+  if (length === 0 || length === 1) return true
+  let dp = new Set()
+  dfs(0)
+
+  function dfs(cur) {
+    dp.add(cur)
+    let tmp = rooms[cur];
+    for (let i = 0; i < tmp.length; i++) {
+      if (!dp.has(tmp[i])) {
+        dfs(tmp[i])
+      }
+    }
+  }
+  return dp.size === rooms.length
 };
 
 console.log(canVisitAllRooms(
