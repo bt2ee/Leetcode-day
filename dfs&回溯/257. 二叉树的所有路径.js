0806b9cf87e0617697904ed2654588bd509de252
chenle <chenle@forchange.tech>
2020-09-04 15:27:41 +0800

    feat: 257. 二叉树的所有路径

--- dfs&回溯/257. 二叉树的所有路径.js
+++ dfs&回溯/257. 二叉树的所有路径.js
@@ -30,3 +30,24 @@ var dfs = (root, temp, result) => {
     dfs(root.right, temp, result)
   }
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  if (root === null) return [];
+  if (root.left === null && root.right === null) {
+    return [root.val.toString()];
+  }
+  var left = binaryTreePaths(root.left),
+    right = binaryTreePaths(root.right);
+  return left.concat(right).map(x => root.val + '->' + x);
+};
