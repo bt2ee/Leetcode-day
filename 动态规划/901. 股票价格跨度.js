6a14f24fec153cdc2d2d4cc539c50145a1474f37
chenle <chenle@forchange.tech>
2020-09-08 22:14:25 +0800

    feat: 901. 股票价格跨度

---
+++ 动态规划/901. 股票价格跨度.js
@@ -0,0 +1,24 @@
+var StockSpanner = function() {
+  this.data = []
+};
+
+/**
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+  let base = 1,
+    offset = this.data.length - 1
+  while (offset >= 0 && price >= this.data[offset]["price"]) {
+    base += this.data[offset]["offset"]
+    offset -= this.data[offset]["offset"]
+  }
+  this.data.push({ 'price': price, 'offset': base })
+  return base
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
