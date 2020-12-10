2032665a16023c7ab856b8916657a8740009db14
chenle <chenle@forchange.tech>
2020-12-10 14:57:47 +0800

    feat: 860. 柠檬水找零

--- 每日一题/860. 柠檬水找零.js
+++ 每日一题/860. 柠檬水找零.js
@@ -4,23 +4,21 @@
  */
 var lemonadeChange = function(bills) {
   let five = 0,
-    ten = 0
-  if (bills.length === 0) return false
+    ten = 0;
+  if (bills.length === 0) return false;
   for (let i of bills) {
     if (i === 5) {
-      five++
+      five++;
     } else if (i === 10) {
-      ten++
-      five--
+      ten++;
+      five--;
+    } else if (ten > 0) {
+      ten--;
+      five--;
     } else {
-      if (ten > 0) {
-        ten--
-        five--
-      } else {
-        five -= 3
-      }
+      five -= 3;
     }
-    if (five < 0) return false
+    if (five < 0) return false;
   }
-  return five >= 0
+  return five >= 0;
 };
